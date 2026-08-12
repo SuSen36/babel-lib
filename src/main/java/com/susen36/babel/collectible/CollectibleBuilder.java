05987da2138794b72416548ee02373c4211f616c
MySQLdisappoint <mysqldisappoint@gmali.com>
2026-08-12 23:10:32 +0800

    fix factory class constructor

--- src/main/java/com/susen36/babel/collectible/CollectibleBuilder.java
+++ src/main/java/com/susen36/babel/collectible/CollectibleBuilder.java
@@ -13,8 +13,14 @@ public class CollectibleBuilder {
     private String MODID = "fuck idea, how could this var be null?";
     private final DeferredRegister<Item> ITEMS = DeferredRegister.create(BuiltInRegistries.ITEM, MODID);
 
+    private CollectibleBuilder() {
+    }
     public CollectibleBuilder(@NotNull String modid) {
         this.MODID = modid;
+    }
+
+    public static CollectibleBuilder create() {
+        return new CollectibleBuilder();
     }
 
     public DeferredHolder<Item, Item> registerCollectible(String name, CollectibleItem.CollectibleEffect effect, boolean consumeSelf) {
