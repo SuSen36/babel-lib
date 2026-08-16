9401b01d3d717be8a5701affdf662dd46f1a79b5
SuSen36 <linchenchen36@gmail.com>
2026-08-16 18:45:02 +0800

    减魔抗效果

---
+++ src/main/java/com/susen36/babel/effect/WeakMagicMobEffect.java
@@ -0,0 +1,35 @@
+package com.susen36.babel.effect;
+
+import com.susen36.babel.init.BabelAttributes;
+import com.susen36.babel.init.BabelMobEffects;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+
+public class WeakMagicMobEffect extends MobEffect {
+    private static final int DEFAULT_DURATION_TICKS = 300;
+
+    public WeakMagicMobEffect() {
+        super(MobEffectCategory.HARMFUL, -10079488);
+        this.addAttributeModifier(BabelAttributes.MAGIC_RESISTANCE.get(), ResourceLocation.fromNamespaceAndPath("babel", "weak_magic_resistance"), -1, AttributeModifier.Operation.ADD_VALUE);
+    }
+
+    public static void apply(LivingEntity entity) {
+        apply(entity, 1, DEFAULT_DURATION_TICKS);
+    }
+
+    public static void apply(LivingEntity entity, int level, int durationTicks) {
+        if (entity.level().isClientSide()) return;
+        MobEffectInstance effect = entity.getEffect(BabelMobEffects.WEAK_MAGIC);
+        int amplifier = effect == null ? 0 : effect.getAmplifier() + level;
+        entity.addEffect(new MobEffectInstance(BabelMobEffects.WEAK_MAGIC, durationTicks, amplifier, false, true, true));
+    }
+
+    @Override
+    public boolean shouldApplyEffectTickThisTick(int duration, int amplifier) {
+        return true;
+    }
+}
